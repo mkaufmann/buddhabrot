4dd7a9a9219241b242f356c19134858939926b0f
Moritz Kaufmann <moritz.kaufmann@outlook.com>
2018-12-25 02:01:17 +0100

    Output more information, also for benchmarking

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::time::Instant;
 
 fn collect(point: ImaginaryNumber, iterations: u64, histogram: &mut Histogram) {
     let mut current = ImaginaryNumber::new(0.0, 0.0);
@@ -31,16 +32,16 @@ fn main() {
     // Buddha sampling constants
     let real_range = (-2.0, 1.0);
     let imaginary_range = (-1.4, 1.4);
-    let limit = 12000;
+    let limit = 50000;
     let bailout = 4.0;
-    let min_iterations = 5;
+    let min_iterations = 10000;
 
     // Image constants
     let resolution = (1920, 1920);
 
     // Runtime constants
     let num_cores = 4;
-    let chunk_size = 20000;
+    let chunk_size = 10;
     let base_seed = 1988;
 
     let (tx, rx) = mpsc::channel();
@@ -52,15 +53,16 @@ fn main() {
             loop {
                 // Find qualifying points
                 let mut points = vec![];
+                let mut probed = 0;
                 while points.len() < chunk_size {
                     let point = random_points.sample();
-
                     escapes(point, limit, bailout)
                         .filter(|&iteration| iteration > min_iterations)
                         .map(|iteration| points.push((point, iteration)));
+                    probed += 1;
                 }
 
-                if thread_tx.send(points).is_err() {
+                if thread_tx.send((points, probed)).is_err() {
                     return;
                 }
             }
@@ -80,7 +82,10 @@ fn main() {
 
     let mut histogram = Histogram::new(resolution.0, resolution.1, -2.0, 2.0, -2.0, 2.0);
     let mut count = 0;
-    for points in rx {
+    let mut total_probed = 0;
+    let start = Instant::now();
+    for (points, probed) in rx {
+        total_probed += probed;
         for (point, iterations) in points {
             collect(point, iterations, &mut histogram);
             if iterations > 5000 {
@@ -90,11 +95,13 @@ fn main() {
         }
 
         count += 1;
-        if (count % 50) == 0 {
-            println!("Saving");
+        if (count % 5) == 0 {
+            println!("Saving {}: {}s", count, start.elapsed().as_secs());
             save_to_image("output/test.png", &mut histogram.data, resolution).unwrap();
         }
-        if count == 10000 {
+        if count == 200 {
+            println!("Duration: {}s", start.elapsed().as_secs());
+            println!("Found: {}, probed: {}", chunk_size*count, total_probed);
             return;
         }
     }
